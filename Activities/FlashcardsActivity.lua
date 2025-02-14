53a6461777934d41a142f43a35892b9e2126f5b4
Ben <operationhope202@gmail.com>
2025-02-14 11:08:37 -0500

    flashcard activity done

--- Activities/FlashcardsActivity.lua
+++ Activities/FlashcardsActivity.lua
@@ -6,13 +6,14 @@ function FlashcardActivity()
         print("In FlashcardActivity() SetData is reporting as: "..tostring(SetData))
         return
     end
-    CenterText(0,scaling(-450,1080,Settings.YRes),SetTitle,Exo32Bold)
-    CenterText(0,scaling(-400,1080,Settings.YRes),tostring(FlashCardActivityFlashCard).."/"..tostring(#SetData),Exo32Bold)
+    N5BoxHighlight(830, 143, 240, 50, true, {255,255,255}, true, Exo32Bold, tostring(FlashCardActivityFlashCard).."/"..tostring(#SetData))
+    N5BoxHighlight(660, 198, 600, 50, true, {255,255,255}, true, Exo32Bold, SetTitle)--145, 90
     --CenterText(0,0,tostring(SetData[2][2]),Exo24)
     DisplayFlashCard(scaling(578,1920,Settings.XRes),scaling(308,1080,Settings.YRes),scaling(763,1920,Settings.XRes),scaling(464,1080,Settings.YRes),tostring(SetData[FlashCardActivityFlashCard][FlashCardActivityFlashCardSide]),Exo24Bold)
     FlashCardKeyboardControls()
-    ButtonStyle1Mod3(578, 800, 763, 100, "Swap Display Order", Exo24Bold, true, "FlashCardChangeDisplaySideFirst()")
+    N5Button(578, 800, 763, 100, true, "FlashCardChangeDisplaySideFirst()", true, Exo24Bold,"Swap Display Order")
     --ButtonStyle1Mod3(1341,308,500,464,"Note here", Exo24Bold, true) !! Add note thing here
+    --!! flickering on flashcard flip sides
 end
 function DisplayFlashCard(BoxX, BoxY, BoxW, BoxH, Text, TextFont)
     if BoxX==nil or BoxY==nil or BoxW==nil or BoxH==nil or Text==nil or TextFont==nil then
@@ -24,6 +25,14 @@ function DisplayFlashCard(BoxX, BoxY, BoxW, BoxH, Text, TextFont)
         print("In DisplayFlashCard() TextFont is reporting as: "..tostring(TextFont))
         return
     end
+    if FlashCardActivityFlashCardSide==1 then
+        love.graphics.setColor(255, 255, 255)
+    else
+        love.graphics.setColor(180, 180, 180)
+    end
+    --95 box background
+    love.graphics.rectangle("fill", BoxX, BoxY, BoxW, BoxH)
+    love.graphics.setColor(0,0,0)--text color
     love.graphics.setFont(TextFont)
     local TH = TextFont:getHeight() * #Text / BoxW -- Estimate height based on wrapping
     local _, wrappedText = TextFont:getWrap(Text, BoxW)
@@ -37,11 +46,6 @@ function DisplayFlashCard(BoxX, BoxY, BoxW, BoxH, Text, TextFont)
     
     love.graphics.printf(Text, BoxX, textY, BoxW, "center")  -- Print wrapped and centered text
     love.graphics.setLineWidth(MediumLine)
-    if FlashCardActivityFlashCardSide==1 then
-        love.graphics.setColor(255, 255, 255)
-    else
-        love.graphics.setColor(255, 153, 0)
-    end
     if Selected then
         function love.mousepressed(x, y, button, istouch, presses)
             if button == 1 then  -- Check for left mouse button
@@ -49,9 +53,16 @@ function DisplayFlashCard(BoxX, BoxY, BoxW, BoxH, Text, TextFont)
             end
         end
     end
-    love.graphics.rectangle("line", BoxX, BoxY, BoxW, BoxH)
-    love.graphics.setLineWidth(ThinLine)
+    --95 box border
+    love.graphics.setLineWidth(MediumLine)
+    love.graphics.setColor(255, 255, 255) -- white
+    love.graphics.line( BoxX, BoxY+BoxH, BoxX+BoxW, BoxY+BoxH) -- horizontal bottom
+    love.graphics.line( BoxX+BoxW, BoxY, BoxX+BoxW, BoxY+BoxH) -- vertical right
+    love.graphics.setColor(0, 0, 0) -- black
+    love.graphics.line( BoxX, BoxY, BoxX+BoxW, BoxY) -- horizontal top
+    love.graphics.line( BoxX, BoxY, BoxX, BoxY+BoxH) -- vertical left
     love.graphics.setColor(255, 255, 255)
+    love.graphics.setLineWidth(1)
 end
 function FlashCardKeyboardControls()
     if ButtonDebounce("left", 0.5) or YScroll>0 then
