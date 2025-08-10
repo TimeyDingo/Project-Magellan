f71027c6da2d2433805747582af6953357785e21
Ben <operationhope202@gmail.com>
2025-08-10 10:41:38 -0400

    icon removed

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.window.title = "M.O.S.S"
-    t.window.icon = "icon.jpg"
+    t.window.icon = nil
     t.window.width = 1024
     t.window.height = 576
     t.window.borderless = false
